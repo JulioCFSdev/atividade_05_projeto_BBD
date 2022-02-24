f2c3b4416470cf68254c4517cf1e1cbc8b884bd4
Tiago Farias Barbosa <99101953+tfarias88@users.noreply.github.com>
2022-02-24 16:33:20 -0300

    Adição da função ball_draw() + pequenas alterações

--- ball.py
+++ ball.py
@@ -4,6 +4,7 @@ import pygame
 class Ball(pygame.sprite.Sprite):
 
     def __init__(self):
+        super().__init__()
         # color
         self.color = (255, 255, 255)
         # ball size
@@ -13,11 +14,14 @@ class Ball(pygame.sprite.Sprite):
         self.dx = 1
         self.dy = 1
         # image surface and rectangle
+        self.ball = pygame.Rect(500, 400, 20, 20)
         self.image = pygame.Surface([self.width, self.height])
         self.rect = self.image.get_rect()
-        pygame.draw.rect(self.image, self.color, [0, 0, self.width, self.height])
+
+    def draw_ball(self):
+        pygame.draw.rect(self.image, self.color, self.ball)
 
     # ball movement function
-    def ball_move(self):
+    def move_ball(self):
         self.rect.x += self.dx
         self.rect.y += self.dy
