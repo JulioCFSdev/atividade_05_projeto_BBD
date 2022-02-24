9f60b868bd439488483c78cc5872b5323675ed7f
JulioCFSdev <90019843+JulioCFSdev@users.noreply.github.com>
2022-02-24 16:29:51 -0300

    Adicionando os comentarios para estruturar as partes

--- ball.py
+++ ball.py
@@ -1,23 +1,22 @@
 import pygame
 
-
 class Ball(pygame.sprite.Sprite):
 
     def __init__(self):
-        # color
-        self.color = (255, 255, 255)
-        # ball size
-        self.width = 20
-        self.height = 20
+        # calling the parent class
+        super().__init__()
         # ball velocity
-        self.dx = 1
-        self.dy = 1
-        # image surface and rectangle
-        self.image = pygame.Surface([self.width, self.height])
-        self.rect = self.image.get_rect()
-        pygame.draw.rect(self.image, self.color, [0, 0, self.width, self.height])
+        self.dx = 3
+        self.dy = 3
+        ball_width = 20
+        ball_height = 20
+        self.ball = pygame.Rect(500, 400, ball_width, ball_height)
+
+    def draw_ball(self, color, display):
+        pygame.draw.rect(display, color, self.ball)
+
 
     # ball movement function
-    def ball_move(self):
-        self.rect.x += self.dx
-        self.rect.y += self.dy
+    def move(self):
+        self.ball.x += self.dx
+        self.ball.y += self.dy
--- game.py
+++ game.py
@@ -1,46 +1,72 @@
 import pygame
 import wall 
-from ball import Ball
+import ball
 
-
+# Class to start our game
 class BlocksAge:
 
-
+    # Initial variables and set screen
     def __init__(self):
         self.width = 1000
         self.height = 800
+        self.white = (255, 255, 255)
         self.score = 0
         self.lives = 3
         self.time = "3:00"
         self.clock = pygame.time.Clock()
+
+        self.player_width = 100
+        self.player_heigth = 20
+        self.player_x = 350
+        self.player_y = 650
+
         pygame.init()
         pygame.display.set_caption("Blocks Age")
-
-        wall.__init__("wall")
         self.screen = pygame.display.set_mode((self.width, self.height))
 
-    
+    # Game loop
     def main_loop(self):
         while True:
             self.handle_input()
             self.game_logic()
             self.draw()
 
+
+    # keyboard inputs
     def handle_input(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 quit()
+        # Keywords paddler
+        # Keywords pause(?)
 
-    
+
+    # Mechanics and world rules
     def game_logic(self):
-        bola = Ball()
-        bola.ball_move()
+        bola = ball.Ball() 
+        bola.move() # movement ball
+        # movement paddler
+        # collision ball/paddler
+        # colision ball/blocks
+        # colision ball/wall (left, right, up)
+        # death point - (collision ball/wall down)
 
 
+    # Drawing the screen and its factors
     def draw(self):
         self.screen.fill((0, 0, 0))
+        bola = ball.Ball()
+
+        # draw screen
         wall.hud(self.screen, self.width, self.score, self.lives, self.time, 1.05, 1.15, 2.35, 2, 4, 5.5)
         wall.screen_lines(self.screen, (255, 0, 255), self.width, self.height, 12)
-        Ball()
+
+        # draw ball
+        bola.draw_ball(self.white, self.screen)
+
+        # draw paddler
+        # draw blocks
+        # draw power-ups (?)
+        
         pygame.display.flip()
 
