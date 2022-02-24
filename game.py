fcc9c32f8bf4bb76b4bdc220acfbff6ac6f92854
JulioCFSdev <90019843+JulioCFSdev@users.noreply.github.com>
2022-02-24 13:16:58 -0300

    Adicionando o modulo draw

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 import pygame
-import wall
+import wall 
 from ball import Ball
 
 
@@ -7,18 +7,24 @@ class BlocksAge:
 
 
     def __init__(self):
+        self.width = 1000
+        self.height = 800
+        self.score = 0
+        self.lives = 3
+        self.time = "3:00"
+        self.clock = pygame.time.Clock()
         pygame.init()
         pygame.display.set_caption("Blocks Age")
 
         wall.__init__("wall")
-        self.screen = pygame.display.set_mode((1000, 800))
+        self.screen = pygame.display.set_mode((self.width, self.height))
 
     
     def main_loop(self):
         while True:
             self.handle_input()
             self.game_logic()
-
+            self.draw()
 
     def handle_input(self):
         for event in pygame.event.get():
@@ -29,4 +35,12 @@ class BlocksAge:
     def game_logic(self):
         bola = Ball()
         bola.ball_move()
+
+
+    def draw(self):
+        self.screen.fill((0, 0, 0))
+        wall.hud(self.screen, self.width, self.score, self.lives, self.time, 1.05, 1.15, 2.35, 2, 4, 5.5)
+        wall.screen_lines(self.screen, (255, 0, 255), self.width, self.height, 12)
+        Ball()
+        pygame.display.flip()
 
