85e923db55491524c47d5754147c3414dfe49314
EmanoJcsh <65429505+EmanoelCardosoJr@users.noreply.github.com>
2022-02-26 14:34:01 -0400

    Removing the constants from game.py, adding these in config.py and connecting the 2 files
    - Added time structure to main game loop and updated hud values

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ import wall
 import ball
 import player
 from config import Config
+
 # Shrinking class call characters
 conf = Config()
 paddler = player.Player()
@@ -15,22 +16,31 @@ class EldenBlocks:
     def __init__(self):
         self.score = 0
         self.lives = 3
-        self.time = "3:00"
         self.clock = pygame.time.Clock()
-
         self.ball = ball.create_ball()
         self.ball_velocity = ball.ball_velocity(conf.ball_speed_x, conf.ball_speed_y)
         pygame.init()
+        pygame.time.set_timer(pygame.USEREVENT, 1000)
         pygame.display.set_caption(conf.bg_name)
         self.screen = pygame.display.set_mode((conf.screen_width, conf.screen_height))
 
     # Game loop
     def main_loop(self):
         while True:
+            for event in pygame.event.get():
+                if event.type == pygame.USEREVENT:
+                    conf.time_counter -= 1
+                    conf.time_text = str(conf.time_counter).rjust(
+                        3) if conf.time_counter > 0 else "X"
+                if conf.time_text == "X":
+                    font2 = pygame.font.Font("DSEG14Classic-Bold.ttf", 34)
+                    text = font2.render("GAME OVER", True, (255, 255, 255))
+                    self.screen.blit(text, (conf.screen_width / 2 - 100, conf.screen_height / 2))
+                    pygame.display.flip()
+                    pygame.time.wait(2000)
             self.handle_input()
             self.game_logic()
             self.draw()
-
             pygame.display.flip()
             pygame.display.update()
             self.clock.tick(conf.fps)
@@ -65,8 +75,13 @@ class EldenBlocks:
 
         # draw screen
         wall.bg_run("wall_dependencies/bg_test.png", conf.coord_bg, self.screen)
-        wall.hud(self.screen, conf.screen_width, self.score, self.lives, self.time, conf.pos_money, conf.pos_score, conf.pos_life_icon, conf.pos_life_var, conf.pos_time_icon, conf.pos_time_value)
-        wall.screen_lines(self.screen, conf.pink, conf.screen_width, conf.screen_height, conf.line_size)
+        wall.hud_score(self.screen, conf.screen_width, conf.pos_money, self.score, conf.pos_score)
+        wall.hud_lives(self.screen, conf.screen_width, conf.pos_life_icon, self.lives,
+                       conf.pos_life_var)
+        wall.hud_time(self.screen, conf.screen_width, conf.pos_time_icon, conf.time_text,
+                      conf.pos_time_value)
+        wall.screen_lines(self.screen, conf.pink, conf.screen_width, conf.screen_height,
+                          conf.line_size)
 
         # draw ball
         ball.draw_ball(self.screen, self.ball)
@@ -74,4 +89,3 @@ class EldenBlocks:
         paddler.draw(self.screen, conf.pink)
         # draw blocks
         # draw power-ups (?)
-        
