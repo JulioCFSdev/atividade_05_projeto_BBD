824c1929b2a4f77335299df2a536780c2b68c6d3
EmanoJcsh <65429505+EmanoelCardosoJr@users.noreply.github.com>
2022-02-28 03:09:33 -0400

    Redefined starting function to Menu()

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from game import EldenBlocks
 # Checking if the file is running from main
 if __name__ == "__main__":
     eldenblocks = EldenBlocks()
-    eldenblocks.main_loop()
+    eldenblocks.Menu()
