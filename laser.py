474129facb5339a6aff58889dcf2a2c7e14ed3e8
Emanoel da Conceição Cardoso Junior <65429505+EmanoelCardosoJr@users.noreply.github.com>
2022-03-07 17:51:14 -0400

    Update laser.py

--- laser.py
+++ laser.py
@@ -3,15 +3,18 @@ import config
 
 conf = config.Config()
 
+
 class Laser(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
         self.laser = []
-        self.laser.append(pygame.image.load('Powerup_sprites/sprite_shoot.png'))
+        self.laser.append(
+            pygame.image.load("Powerup_sprites/sprite_shoot.png")
+        )
         self.image = self.laser[0]
         self.image = pygame.transform.scale(self.image, (60, 60))
-        self.rect = self.image.get_rect(center = pos)
+        self.rect = self.image.get_rect(center=pos)
         self.speed = 8
 
-    def update(self): 
+    def update(self):
         self.rect.y -= self.speed
